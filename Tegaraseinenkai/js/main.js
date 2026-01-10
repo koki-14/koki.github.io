baf2fa68b71ba86b759e7db0b4178695741bf1c5
koki <baskoukteki@gmail.com>
2026-01-10 23:40:53 +0900

    up

--- Tegaraseinenkai/js/main.js
+++ Tegaraseinenkai/js/main.js
@@ -5,3 +5,16 @@ function toggleMenu() {
 function closeMenu() {
   document.getElementById("sideMenu").classList.remove("open");
 }
+
+/* スライドショー */
+const slides = [
+  "images/slide1.jpg",
+  "images/slide2.jpg",
+  "images/slide3.jpg"
+];
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  document.getElementById("slideImage").src = slides[index];
+}, 4000);
